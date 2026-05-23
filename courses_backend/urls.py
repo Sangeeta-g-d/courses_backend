0416e9d58814de0c770890a6a0beed6719aae371
Sangeeta-g-d <sangeetadani599.com>
2026-05-23 15:58:58 +0530

    added static in urls

--- courses_backend/urls.py
+++ courses_backend/urls.py
@@ -33,6 +33,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 # Catch-all pattern for 404 (must be last)
 urlpatterns += [
